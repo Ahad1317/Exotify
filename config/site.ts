418f976317c5d212bbb1908939dfa741095766b4
Ahad1713 <khanazan761@gmail.com>
2023-11-18 21:14:57 +0530

    :memo: Lil metadata

--- config/site.ts
+++ config/site.ts
@@ -1,8 +1,8 @@
 export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
-  name: "Name",
-  description: "Description",
+  name: "Exotify",
+  description: "Expertly Designed E-commerce webiste by Ahad",
   footer: [
     { name: "Home", href: "/" },
     { name: "About", href: "#" },
